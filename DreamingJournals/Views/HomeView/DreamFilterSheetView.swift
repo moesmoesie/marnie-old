83bfd50dc1b3d2bab1929a69b88d9a72a2ec3329
moesmoesie <mndarwesh@protonmail.com>
2020-05-15 19:35:51 +0200

    Refactor DreamFilterSheet

--- DreamingJournals/Views/HomeView/DreamFilterSheetView.swift
+++ DreamingJournals/Views/HomeView/DreamFilterSheetView.swift
@@ -13,7 +13,7 @@ struct DreamFilterSheetView: View {
     @Environment(\.presentationMode) var presentationMode
     @FetchRequest(entity: Tag.entity(), sortDescriptors: []) var tags : FetchedResults<Tag>
     @EnvironmentObject var filterObserver : FilterObserver
-
+    
     var allTags : [TagViewModel]{
         return self.tags.map({TagViewModel(tag: $0)})
     }
@@ -38,24 +38,24 @@ struct DreamFilterSheetView: View {
                 ZStack(alignment:.top){
                     self.theme.primaryBackgroundColor.edgesIgnoringSafeArea(.all)
                     ScrollView{
-                        VStack(alignment: .leading){
+                        VStack(alignment: .leading, spacing: 0){
                             
                             self.topBarView
-                            
-                            self.isBookmarkedFilterView
                                 .padding(.horizontal, self.theme.mediumPadding)
+                                .padding(.top, self.theme.mediumPadding)
+                            
+                            ActiveFilters(tags: self.filterObserver.tagFilters)
+                                .padding(.horizontal, self.theme.mediumPadding)
+                                .padding(.top,self.theme.smallPadding)
+                                .frame(minWidth : geo.size.width, minHeight: 200)
+                            
+                            self.seperatorView
                             
                             AvailableFilters(tags: self.availableFilters)
                                 .padding(.horizontal, self.theme.mediumPadding)
+                                .padding(.top, self.theme.mediumPadding)
                                 .frame(width : geo.size.width)
-                            
-                            ActiveFilters(tags: self.activeFilters)
-                                .padding(.horizontal, self.theme.mediumPadding)
-                                .frame(width : geo.size.width)
-                            
                         }
-                        .padding(.leading, self.theme.mediumPadding)
-                        .padding(.bottom, self.theme.smallPadding)
                     }
                 }
         }
@@ -69,7 +69,7 @@ struct DreamFilterSheetView: View {
     
     private var isBookmarkedFilterView : some View{
         Toggle(isOn: self.$filterObserver.showOnlyFave){
-            Text("Favorite only").font(theme.primaryLargeFont).foregroundColor(theme.textTitleColor)
+            EmptyView()
         }
     }
     
@@ -77,9 +77,9 @@ struct DreamFilterSheetView: View {
         HStack(alignment:.center, spacing: theme.mediumPadding){
             titleView
             Spacer()
+            isBookmarkedFilterView
             closeButtonView
-        }.padding(.horizontal, theme.mediumPadding)
-            .padding(.top, theme.mediumPadding)
+        }
     }
     
     private var titleView : some View{
@@ -98,6 +98,8 @@ struct DreamFilterSheetView: View {
                 .foregroundColor(theme.passiveLightColor)
         }
     }
+    
+    
 }
 
 // MARK: - Active Filters
@@ -105,15 +107,32 @@ struct DreamFilterSheetView: View {
 private struct ActiveFilters : View {
     let tags : [TagViewModel]
     @EnvironmentObject var filterObserver : FilterObserver
-    
+    @EnvironmentObject var theme : Theme
+
     var body: some View{
-        CollectionView(data: tags, animate: true) { (tag : TagViewModel) in
-            TagView(tag: tag)
-                .onTapGesture {
-                    let index = self.filterObserver.tagFilters.firstIndex(of: tag)!
-                    self.filterObserver.tagFilters.remove(at: index)
+        ZStack(alignment: .center){
+            if self.tags.isEmpty{
+                self.placeHolderView
+            }else{
+                VStack{
+                    CollectionView(data: tags, animate: true) { (tag : TagViewModel) in
+                        TagView(tag: tag)
+                            .onTapGesture {
+                                let index = self.filterObserver.tagFilters.firstIndex(of: tag)!
+                                self.filterObserver.tagFilters.remove(at: index)
+                        }
+                    }
+                    Spacer()
+                }
             }
         }
+    }
+    
+    private var placeHolderView : some View {
+        Text("No Active Filters")
+            .foregroundColor(theme.textTitleColor)
+            .opacity(0.5)
+            .offset(x: 0, y: -theme.smallPadding)
     }
 }
 
@@ -122,13 +141,34 @@ private struct ActiveFilters : View {
 private struct AvailableFilters : View {
     let tags : [TagViewModel]
     @EnvironmentObject var filterObserver : FilterObserver
+    @EnvironmentObject var theme : Theme
     
+    
+    var availableFilters : [TagViewModel]{
+          var filters : [TagViewModel] = []
+          for filter in tags{
+              if !filters.contains(where: {filter.text == $0.text}) && !filterObserver.tagFilters.contains(where: {filter.text == $0.text}){
+                  filters.append(filter)
+              }
+          }
+          return filters
+      }
+
     var body: some View{
-        CollectionView(data: tags, animate: true) { (tag : TagViewModel) in
-            TagView(tag: tag).onTapGesture {
-                self.filterObserver.tagFilters.append(tag)
+        VStack(alignment: .leading){
+            title
+            CollectionView(data: tags, animate: true) { (tag : TagViewModel) in
+                TagView(tag: tag).onTapGesture {
+                    self.filterObserver.tagFilters.append(tag)
+                }
             }
         }
+    }
+    
+    private var title : some View{
+        Text("Available Filters")
+            .foregroundColor(theme.textTitleColor)
+            .font(theme.primaryLargeFont)
     }
 }
 
