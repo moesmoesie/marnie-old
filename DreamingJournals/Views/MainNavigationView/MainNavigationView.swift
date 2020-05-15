873251d714febd7f8dc02aa953e652f6b350bbf3
moesmoesie <mndarwesh@protonmail.com>
2020-05-15 12:22:17 +0200

    Refactor MainNavigationView

--- DreamingJournals/Views/MainNavigationView/MainNavigationView.swift
+++ DreamingJournals/Views/MainNavigationView/MainNavigationView.swift
@@ -11,68 +11,73 @@ import SwiftUI
 struct MainNavigationView: View {
     @EnvironmentObject var theme : Theme
     @EnvironmentObject var navigationObserver : NavigationObserver
+    
     var body: some View {
         ZStack(alignment:.bottom){
             if navigationObserver.currentPage == Pages.home{
                 HomeView().padding(.bottom,  navigationObserver.showBottomBar ? getBottomSaveArea() : 0)
             }else if navigationObserver.currentPage == Pages.settings{
-                SettingsView()
+                SettingsView().padding(.bottom,  navigationObserver.showBottomBar ? getBottomSaveArea() : 0)
             }else if navigationObserver.currentPage == Pages.statistics{
                 theme.primaryBackgroundColor.edgesIgnoringSafeArea(.all)
             }
             
-            VStack(spacing : 0){
-                BottomAppBar()
-                theme.primaryBackgroundColor.frame(height: getBottomSaveArea())
-            }
-            .animation(nil)
-            .offset(x: 0, y: navigationObserver.showBottomBar ? 0 : 100)
-            .disabled(!navigationObserver.showBottomBar)
-            .animation(.easeInOut)
+            BottomAppBar()
         }.edgesIgnoringSafeArea(.bottom)
-        
     }
 }
 
-struct MainNavigationView_Previews: PreviewProvider {
-    static var previews: some View {
-        MainNavigationView()
-    }
-}
-
+//MARK: - BOTTOM APP BAR
 
 private struct BottomAppBar : View {
     @EnvironmentObject var theme : Theme
     @EnvironmentObject var navigationObserver : NavigationObserver
-
+    
     var body : some View{
         return
             VStack(spacing: 0){
-                Rectangle().frame(height: 0.2).foregroundColor(theme.passiveDarkColor)
-                HStack{
-                    Spacer()
-                    BarButton(page: Pages.home, iconName: "house.fill")
-                    Spacer()
-                    Spacer()
-                    BarButton(page: Pages.statistics, iconName: "chart.bar.fill")
-                    Spacer()
-                    Spacer()
-                    BarButton(page: Pages.settings, iconName: "gear")
-                    Spacer()
-                }.padding(.vertical, theme.smallPadding * 1.2)
+                topBorder
+                
+                bottomBarcontent
+                    .padding(.vertical, theme.smallPadding * 1.2)
+                    .padding(.bottom, getBottomSaveArea())
                     .background(theme.primaryBackgroundColor)
+                   
+            }
+            .offset(x: 0, y: navigationObserver.showBottomBar ? 0 : 100)
+            .disabled(!navigationObserver.showBottomBar)
+            .animation(.easeInOut)
+    }
+    
+    var topBorder : some View{
+        Rectangle()
+            .frame(height: 0.2)
+            .foregroundColor(theme.passiveDarkColor)
+    }
+    
+    var bottomBarcontent : some View{
+        HStack{
+            Spacer()
+            BarButton(page: Pages.home, iconName: "house.fill")
+            Spacer()
+            Spacer()
+            BarButton(page: Pages.statistics, iconName: "chart.bar.fill")
+            Spacer()
+            Spacer()
+            BarButton(page: Pages.settings, iconName: "gear")
+            Spacer()
         }
     }
     
     func BarButton(page : Pages, iconName: String) -> some View {
-        Button(action: {
+        let color = navigationObserver.currentPage == page ? theme.primaryColor : theme.passiveLightColor
+        return Button(action: {
             if self.navigationObserver.currentPage != page{
                 self.navigationObserver.currentPage = page
             }
         }){
-            Image(systemName: iconName).foregroundColor(navigationObserver.currentPage == page ? theme.primaryColor : theme.passiveLightColor)
+            Image(systemName: iconName)
+                .foregroundColor(color)
         }
     }
 }
-
-
