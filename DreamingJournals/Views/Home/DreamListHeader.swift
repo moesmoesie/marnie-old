1901bc19fa8c1dbb43612320c4f356445570403c
moesmoesie <mndarwesh@protonmail.com>
2020-05-24 12:13:12 +0200

    Implement filter buttons

--- DreamingJournals/Views/Home/DreamListHeader.swift
+++ DreamingJournals/Views/Home/DreamListHeader.swift
@@ -7,23 +7,78 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ListHeader : View {
-    
-    
     @EnvironmentObject var filterObserver : FilterObserver
     @Environment(\.managedObjectContext) var moc
+    let lucidFilter = FilterViewModel(filter: .lucid(true))
+    let nightmareFilter = FilterViewModel(filter: .nightmare(true))
+    let bookmarkedFilter = FilterViewModel(filter: .bookmarked(true))
+    @State var showFilterSheet = false
     
     var body: some View{
         return
-            HStack(spacing: 0){
-                title.padding(.leading, .extraSmall)
-                Spacer()
-                FilterButtonView()
-                AddDreamButtonView()
+            VStack(alignment: .leading, spacing: 0){
+                HStack(spacing: 0){
+                    title.padding(.leading, .extraSmall)
+                    Spacer()
+                }
+                HStack{
+                    
+                    FilterButton(iconName: "heart") {
+                                       if let index = self.getFilterIndex(filter: self.bookmarkedFilter){
+                                           self.filterObserver.filters.remove(at: index)
+                                       }else{
+                                           self.filterObserver.filters.append(self.bookmarkedFilter)
+                                       }
+                                   }
+                    
+                    Spacer()
+                    
+                    FilterButton(iconName: "eye") {
+                        if let index = self.getFilterIndex(filter: self.lucidFilter){
+                            self.filterObserver.filters.remove(at: index)
+                        }else{
+                            self.filterObserver.filters.append(self.lucidFilter)
+                        }
+                    }
+                                
+                    
+                    Spacer()
+                    
+                    FilterButton(iconName: "tropicalstorm") {
+                        if let index = self.getFilterIndex(filter: self.nightmareFilter){
+                            self.filterObserver.filters.remove(at: index)
+                        }else{
+                            self.filterObserver.filters.append(self.nightmareFilter)
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    FilterButton(iconName: "tag") {
+                        self.showFilterSheet = true
+                    }.sheet(isPresented: self.$showFilterSheet){
+                        DreamFilterSheetView()
+                            .environmentObject(self.filterObserver)
+                            .environment(\.managedObjectContext, self.moc)
+                    }
+                }.padding(.horizontal, .small)
         }
+        
+        
+        
     }
     
+    func getFilterIndex(filter : FilterViewModel) -> Int?{
+        if let index = self.filterObserver.filters.firstIndex(where: { (checkFilter : FilterViewModel) in
+            checkFilter.filter.areEqual(filter: filter.filter)
+        }){
+            return index
+        }
+        return nil
+    }
     
     //MARK: - HELPER VIEWS
     
@@ -34,51 +89,38 @@ struct ListHeader : View {
     }
 }
 
-
-struct FilterButtonView : View {
-    @State var showSheet = false
+struct FilterButton : View{
+    let action : () -> ()
+    let iconName : String
     
-    @EnvironmentObject var filterObserver : FilterObserver
-    @Environment(\.managedObjectContext) var moc
+    init(iconName : String, action : @escaping () -> ()) {
+        self.action = action
+        self.iconName = iconName
+    }
     
-    var body : some View{
-        Button(action: {
-            mediumFeedback()
-            self.showSheet = true
-        }){
-            Image(systemName: "magnifyingglass.circle.fill")
-                .foregroundColor(filterObserver.filters.isEmpty ? Color.main2 : Color.accent1)
-                .font(.largeTitle)
-                .background(Color.background1)
-        }.sheet(isPresented: $showSheet){
-                DreamFilterSheetView()
-                     .environmentObject(self.filterObserver)
-                     .environment(\.managedObjectContext, self.moc)
-        }.buttonStyle(PlainButtonStyle())
+    var body: some View{
+        Image(systemName: iconName)
+            .imageScale(.large)
+            .foregroundColor(.main1)
+            .frame(width: .extraLarge + 20, height: .extraLarge + 20)
+            .background(Color.background1)
+            .cornerRadius(10)
+            .secondaryShadow()
+            .onTapGesture(perform: action)
     }
 }
 
-struct AddDreamButtonView : View {
-    @State var showNewDream = false
-    
-    var body : some View{
+struct DreamListHeader_Previews: PreviewProvider {
+    static var context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+    static var previews: some View {
         ZStack{
-            NavigationLink(destination: DreamDetailView(dream: DreamViewModel()), isActive: self.$showNewDream){
-                EmptyView()
-            }.hidden().disabled(true).frame(width:.zero, height: .zero)
-            Button(action: {
-                mediumFeedback()
-                self.showNewDream = true
-            }){
-                Image(systemName: "plus.circle.fill")
-                    .foregroundColor(.accent2)
-                    .font(.largeTitle)
-                    .background(Color.background2)
-                    .padding(.medium)
-            }.buttonStyle(PlainButtonStyle())
+            Color.background1.edgesIgnoringSafeArea(.all)
+            VStack {
+                ListHeader()
+                    .environmentObject(FilterObserver())
+                    .environment(\.managedObjectContext, context)
+                Spacer()
+            }
         }
     }
 }
-
-
-
