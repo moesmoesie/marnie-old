ac2471dbca42a15fe0d34d5815c4a8d8efe0c50a
moesmoesie <mndarwesh@protonmail.com>
2020-05-25 14:18:53 +0200

    Put BackButton in seperate Struct

--- DreamingJournals/Views/DreamDetailView/DreamDetailTopBar.swift
+++ DreamingJournals/Views/DreamDetailView/DreamDetailTopBar.swift
@@ -9,50 +9,43 @@
 import SwiftUI
 
 struct DreamDetailTopBar: View {
-    @EnvironmentObject var keyboardObserver : KeyboardObserver
-    @EnvironmentObject var dream : DreamViewModel
-    
-    @Environment(\.managedObjectContext) var moc
-    @Environment(\.presentationMode) var presentationMode
-    @State var showAlert : Bool = false
-    @State var currentAlert : Alert = Alert(title: Text("Error"))
-    
-    
     var body: some View {
         HStack(alignment : .center,spacing : .medium){
-            backView
+            BackButton()
             Spacer()
         }
     }
-    
-    //MARK: - Helper Views
-    var backView : some View{
+}
+
+
+private struct BackButton : View{
+    @EnvironmentObject var dream : DreamViewModel
+    @EnvironmentObject var keyboardObserver : KeyboardObserver
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.presentationMode) var presentationMode
+    @State var showAlert = false
+
+    var body: some View{
         Button(action:backButtonPress){
             Image(systemName: "chevron.left").foregroundColor(.accent2)
                 .padding(.vertical, .small)
                 .padding(.horizontal, .medium)
-        }.alert(isPresented: $showAlert) {
-            self.currentAlert
-        }
-    }
-
-    //MARK: - ALERTS
-    func unsavedChangesAlert() -> Alert {
-        Alert(title: Text("Unsaved Changes"), message: Text("You have made changes without saving!"), primaryButton: .destructive(Text("Discard"), action:{
-            self.presentationMode.wrappedValue.dismiss()
-        }), secondaryButton: .cancel())
+        }.alert(isPresented: $showAlert, content: unsavedChangesAlert)
     }
     
-    //MARK: - Logic Funtions
-
     func backButtonPress(){
         let dreamService = DreamService(managedObjectContext: self.moc)
         keyboardObserver.dismissKeyboard()
         if dreamService.checkForChanges(dream){
-            self.currentAlert = unsavedChangesAlert()
             self.showAlert = true
         }else{
             self.presentationMode.wrappedValue.dismiss()
         }
+    }
+    
+    func unsavedChangesAlert() -> Alert {
+        Alert(title: Text("Unsaved Changes"), message: Text("You have made changes without saving!"), primaryButton: .destructive(Text("Discard"), action:{
+            self.presentationMode.wrappedValue.dismiss()
+        }), secondaryButton: .cancel())
     }
 }
