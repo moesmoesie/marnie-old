a0aac3de615b40519b41cab6a6b2f2c0c36a78a9
moesmoesie <mndarwesh@protonmail.com>
2020-05-25 14:51:45 +0200

    Add save button to DreamDetailTopBar

--- DreamingJournals/Views/DreamDetailView/DreamDetailTopBar.swift
+++ DreamingJournals/Views/DreamDetailView/DreamDetailTopBar.swift
@@ -10,10 +10,49 @@ import SwiftUI
 
 struct DreamDetailTopBar: View {
     var body: some View {
-        HStack(alignment : .center,spacing : .medium){
+        return HStack(alignment : .center,spacing : .medium){
             BackButton()
             Spacer()
+            SaveButton()
         }
+    }
+}
+
+private struct SaveButton : View{
+    @EnvironmentObject var dream : DreamViewModel
+    @EnvironmentObject var keyboardObserver : KeyboardObserver
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.presentationMode) var presentationMode
+    @State var showAlert = false
+    @State var message = ""
+    
+    var body: some View{
+        Button(action:saveDream){
+            Text(dream.isNewDream ?  "Save" : "Update")
+                .foregroundColor(.main1)
+                .font(.primaryLarge)
+                .padding(.trailing, .medium)
+        }.buttonStyle(PlainButtonStyle())
+            .alert(isPresented: $showAlert, content: InvalidSaveAlert)
+    }
+    
+    func saveDream(){
+        mediumFeedback()
+        self.showAlert = false
+        let dreamService = DreamService(managedObjectContext: self.moc)
+        do {
+            try dreamService.saveDream(dreamViewModel: dream)
+            presentationMode.wrappedValue.dismiss()
+        } catch DreamService.DreamError.invalidSave(error: let message) {
+            self.message = message
+            self.showAlert = true
+        } catch{
+            print("Unexpected error: \(error).")
+        }
+    }
+    
+    func InvalidSaveAlert() -> Alert{
+        Alert(title: Text(self.dream.isNewDream ? "Invalid Save" : "Invalid Update"), message: Text(self.message), dismissButton: .default(Text("OK")))
     }
 }
 
@@ -24,7 +63,7 @@ private struct BackButton : View{
     @Environment(\.managedObjectContext) var moc
     @Environment(\.presentationMode) var presentationMode
     @State var showAlert = false
-
+    
     var body: some View{
         Button(action:backButtonPress){
             Image(systemName: "chevron.left").foregroundColor(.accent2)
