1c54951619dd56aac60d7d06000463b943177f8b
moesmoesie <mndarwesh@protonmail.com>
2020-05-25 14:13:46 +0200

    Remove unused views from DreamDetailTopBar

--- DreamingJournals/Views/DreamDetailView/DreamDetailTopBar.swift
+++ DreamingJournals/Views/DreamDetailView/DreamDetailTopBar.swift
@@ -10,9 +10,7 @@ import SwiftUI
 
 struct DreamDetailTopBar: View {
     @EnvironmentObject var keyboardObserver : KeyboardObserver
-    
     @EnvironmentObject var dream : DreamViewModel
-    @EnvironmentObject var editorObserver : EditorObserver
     
     @Environment(\.managedObjectContext) var moc
     @Environment(\.presentationMode) var presentationMode
@@ -24,53 +22,10 @@ struct DreamDetailTopBar: View {
         HStack(alignment : .center,spacing : .medium){
             backView
             Spacer()
-            tagView
-            bookmarkedView
-            actionButton
-                .padding(.trailing, .medium)
         }
     }
     
     //MARK: - Helper Views
-    
-    var actionButton : some View{
-        Button(action: {
-            mediumFeedback()
-            self.editorObserver.currentMode = .actionMode
-        }){
-            Image(systemName: "ellipsis").foregroundColor(.accent2)
-                .padding(.vertical, .small)
-        }
-    }
-    
-    var tagView : some View{
-        Button(action: {
-            mediumFeedback()
-            withAnimation{
-                if self.editorObserver.isInTagMode{
-                    self.editorObserver.currentMode = .regularMode
-                }else{
-                    self.editorObserver.currentMode = .tagMode
-                }
-            }
-            
-        }){
-            Image(systemName: "tag.fill").foregroundColor(.accent2)
-                .padding(.vertical, .small)
-        }
-    }
-    
-    var bookmarkedView : some View{
-        Button(action:{
-            mediumFeedback()
-            self.dream.isBookmarked.toggle()
-        }){
-            Image(systemName: "heart.fill")
-                .foregroundColor(self.dream.isBookmarked ? .accent1 : .secondary)
-                .padding(.vertical, .small)
-        }
-    }
-    
     var backView : some View{
         Button(action:backButtonPress){
             Image(systemName: "chevron.left").foregroundColor(.accent2)
